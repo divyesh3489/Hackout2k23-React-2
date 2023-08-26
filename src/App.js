8db9988b27ee4dc7d11d6b1c01539239461f95b8
divyesh3489 <99518441+divyesh3489@users.noreply.github.com>
2023-08-26 09:50:58 +0530

    cp1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
   return (
-    <></>
+    <>
+    <Router>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 } 
 
